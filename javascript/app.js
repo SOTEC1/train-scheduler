7cd9490697b48c489688aafde6c934bcb6c3f1e6
Cotes <cotes.brandon@yahoo.com>
2019-01-10 18:58:00 -0500

    scheduler working

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
   // Initialize Firebase
   var config = {
@@ -14,63 +14,76 @@ $(document).ready(function() {
 
   // save firebase database reference
   var database = firebase.database();
-  
+
 
   // add event listener for form submit
-  $("#submit-btn").on("click", function(event) {
+  $("#submit-btn").on("click", function (event) {
     event.preventDefault();
 
     var employeeData = {
       name: $("#name-input").val().trim(),
-      role: $("#role-input").val().trim(),
-      startDate: $("#start-input").val(),
-      rate: parseInt($("#rate-input").val())
+      destination: $("#destination-input").val().trim(),
+      startTime: parseInt($("#start-input").val()),
+      frequency: parseInt($("#frequency-input").val())
     };
-
-    // convert employee start date to unix to store unix time in DB
-    employeeData.startDate = moment(employeeData.startDate, "YYYY-MM-DD").format("X");
 
     // add employee to firebase using .push() method instead of .set()
     database.ref().push(employeeData);
 
     // clear out any value in form input tags on page
     $("#name-input").val("");
-    $("#role-input").val("");
+    $("#destination-input").val("");
     $("#start-input").val("");
-    $("#rate-input").val("");
+    $("#frequency-input").val("");
 
   });
 
   // set up child_added event listener for firebase to send new information every time an employee is added and when the page loads
-  database.ref().on("child_added", function(childSnapshot) {
+  database.ref().on("child_added", function (childSnapshot) {
     console.log(childSnapshot.val());
     var name = childSnapshot.val().name;
-    var role = childSnapshot.val().role;
-    var rate = childSnapshot.val().rate;
-    var startDate = childSnapshot.val().startDate;
+    var destination = childSnapshot.val().destination;
+    var frequency = childSnapshot.val().frequency;
+    var startTime = childSnapshot.val().startTime;
 
     // if you need the key of the child
     console.log(childSnapshot.key);
-   
-    // convert startDate to pretty print on page (ex. 02/20/1999)
-    var startDateFormatted = moment(startDate, "X").format("MM/DD/YYYY");
 
-    // determine amount of months between startDate and now
-    var totalMonthsWorked = moment().diff(moment(startDate, "X"), "months");
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(startTime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted.format("HH:mm"));
 
-    // multiply number of months worked (calculated above) and monthly rate to get total billed
-    var totalBilled = totalMonthsWorked * rate;
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % frequency;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = frequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("hh:mm");
+    console.log("ARRIVAL TIME: " + nextTrain);
+
 
     // create a table row for my employee
     var $tr = $("<tr>");
     $tr
       .attr("employee-key", childSnapshot.key)
       .append(`<td>${name}</td>`)
-      .append(`<td>${role}</td>`)
-      .append(`<td>${startDateFormatted}</td>`)
-      .append(`<td>${totalMonthsWorked}</td>`)
-      .append(`<td>${rate}</td>`)
-      .append(`<td>$${totalBilled}</td>`);
+      .append(`<td>${destination}</td>`)
+      .append(`<td>${startTime}</td>`)
+      .append(`<td>${frequency}</td>`)
+      .append(`<td>${tMinutesTillTrain}</td>`)
+      .append(`<td>${nextTrain}</td>`);
 
     // select table's body and append employee table row
     $("tbody#employee-info").append($tr);
